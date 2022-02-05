780e37aa2ce58fdfde00340832ea53828454a2ce
gobielumalai <gobielumalais@gmail.com>
2022-02-05 18:07:16 +0530

    project new

---
+++ src/com/gt/CreateGit.java
@@ -0,0 +1,19 @@
+package com.gt;
+
+public class CreateGit {
+
+	public void method() {
+		System.out.println("method name is guna");
+		
+	}
+	public void name() {
+		System.out.println("enter the name");
+		
+		
+	}
+	public static void main(String[] args) {
+		CreateGit g = new CreateGit();
+		g.method();
+		g.name();
+	}
+}
